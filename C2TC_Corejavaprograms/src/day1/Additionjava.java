6101665d84749412896f7e9833e84c6c5418499f
mdimr <mdimr@DESKTOP-6HFGJTM>
2025-06-12 22:01:45 +0530

    My addition java Program

--- C2TC_Corejavaprograms/src/day1/Additionjava.java
+++ C2TC_Corejavaprograms/src/day1/Additionjava.java
@@ -5,14 +5,13 @@ public class Additionjava {
 	public static void main(String[] args) {
 		int a,b,c;
 		Scanner sc;
-		sc = new Scanner(System.in);
-				
+		sc = new Scanner(System.in);		
 		System.out.println("Enter a:");
 		a=sc.nextInt();
 		System.out.println("Enter b:");
 		b=sc.nextInt();
 		c=a+b;
-		System.out.println("Addition of a="+a+" and b="+b+" is"+c);
+		System.out.println("Addition of a="+a+" and b="+b+" is "+c);
 		
 	}
 
