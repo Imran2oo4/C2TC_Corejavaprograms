6ea843cff6dc1025c632b34e41f98bdb872f1632
mdimr <mdimr@DESKTOP-6HFGJTM>
2025-06-14 22:12:22 +0530

    CoreJavaPrograms

---
+++ C2TC_Corejavaprograms/src/day1/IdentifiersDemo.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("Value of the number variable is :" +$number);
+		
+		String studentName = "Imran";
+		System.out.println("Value is : "+studentName);
+
+	}
+
+}
