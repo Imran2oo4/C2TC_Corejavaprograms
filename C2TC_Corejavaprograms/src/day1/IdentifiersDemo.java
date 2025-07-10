a38d1fd428e9721dccd345d17e83cdf0bc85cfe5
mdimr <mdimr@DESKTOP-6HFGJTM>
2025-07-10 09:37:58 +0530

    Corejavaprograms

--- C2TC_Corejavaprograms/src/day1/IdentifiersDemo.java
+++ C2TC_Corejavaprograms/src/day1/IdentifiersDemo.java
@@ -4,7 +4,7 @@ public class IdentifiersDemo {
 
 	public static void main(String[] args) {
 		int $number = 20;
-		System.out.println("Value of the number variable is :" +$number);
+		System.out.println("Value of the number variable is an :" +$number);
 		
 		String studentName = "Imran";
 		System.out.println("Value is : "+studentName);
